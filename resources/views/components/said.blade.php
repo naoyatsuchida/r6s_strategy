75fbca877ef3ea355c52cf35902d80b3824af681
naoyatsuchida <naoyatsuchida9@gmail.com>
2020-06-20 17:45:41 +0900

    マップ固定

---
+++ resources/views/components/said.blade.php
@@ -0,0 +1,3 @@
+<div class="show__said">
+    <h1>{{$title}}</h1>
+</div>
--- resources/views/strategy/index.blade.php
+++ resources/views/strategy/index.blade.php
@@ -5,12 +5,8 @@
   <div class='strategy'>
     <h4>マップを選択してください</h4>
     <div class='strategy__maps'>
-        
         @each('components.map',$maps, 'map')
     </div>
-    
   </div>
 </body>
-
-
 @endsection
