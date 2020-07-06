171783f5e134e9d80c70fd09dcced7cd413f5fb2
naoyatsuchida <naoyatsuchida9@gmail.com>
2020-07-06 21:52:01 +0900

    ビュー修正

--- resources/views/components/map.blade.php
+++ resources/views/components/map.blade.php
@@ -1,6 +1,6 @@
 
 <div class="strategy__maps__img">
-    <span class="map_name">{{$map['name']}}</span>
+    <div class="map_name">{{$map['name']}}</div>
     <a href="{{route('strategy.show',['id'=> $map['id']])}}">
         <img src="{{asset($map['path'])}}" alt="マップ画像">
     </a>
--- resources/views/components/said.blade.php
+++ resources/views/components/said.blade.php
@@ -1,9 +1,11 @@
 <div class="show__said">
-    <button class="btn-danger" id="attack">攻撃</button>
-    or
-    <button class="btn-primary" id="defense">防衛</button>
+    <div class="show__said__btn">
+        <button class="btn-danger" id="attack">攻撃</button>
+        or
+        <button class="btn-primary" id="defense">防衛</button>
+    </div>
    
-    <div class="show__said__attack" style="display: none;">
+    <div class="show__said__attack" >
         @foreach($attackpath as $attack)
             <div class="box" id="attacop" >
                 <img src="{{ asset($attack->path)}}" alt="オペレーター画像" data-color="{{$attack->color}}">
